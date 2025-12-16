dd020d1f0ebb219ad9efcff83aa6b11897c3aeb5
parallels <albusfang962@gmail.com>
2025-12-16 09:30:45 -0500

    added speed_mean speed_var normal_mean normal_var into dataset

--- models/data_mlp.py
+++ models/data_mlp.py
@@ -1,62 +1,71 @@
 import numpy as np
 import torch
 from torch import Tensor
-from torch.autograd import Variable, grad
-#import open3d as o3d
+from torch.autograd import Variable
+
 
 class _numpy2dataset(torch.utils.data.Dataset):
-    def __init__(self, points, speed, normal):
-        # Creating identical pairs
-        points    = Variable(Tensor(points))
-        speed  = Variable(Tensor(speed))
-        normal  = Variable(Tensor(normal))
-        self.data=torch.cat((points,speed,normal),dim=1)
-        #self.grid  = Variable(Tensor(grid))
+    def __init__(self, data):
+        """
+        data: Tensor of shape (N, D)
+        """
+        self.data = Variable(data)
 
-    def send_device(self,device):
-        self.data    = self.data.to(device)
+    def send_device(self, device):
+        self.data = self.data.to(device)
 
     def __getitem__(self, index):
-        data = self.data[index]
-        #print(index)
-        return data, index
+        return self.data[index], index
+
     def __len__(self):
         return self.data.shape[0]
 
+
 def Database(PATH):
-    
-    #try:
-    points = np.load('{}/sampled_points.npy'.format(PATH))#[:100000,:]
-    speed = np.load('{}/speed.npy'.format(PATH))#[:100000,:]
-    normal = np.load('{}/normal.npy'.format(PATH))#[:100000,:]
-    #occupancies = np.unpackbits(np.load('{}/voxelized_point_cloud_128res_20000points.npz'.format(PATH))['compressed_occupancies'])
-    #input = np.reshape(occupancies, (128,)*3)
-    #grid = np.array(input, dtype=np.float32)
-    #print(tau.min())
-    #p0 = np.random.rand(100000,2)-0.5
-    #s0 = sdf(p0)
-    #p1 = np.random.rand(100000,2)-0.5
-    #s1 = sdf(p1)
-    #points = np.concatinate((p0,p1),axis=1)
-    #speed = np.concatinate((s0,s1),axis=1)
-    print(points.shape)
-    print(speed.shape)
-    print(normal.shape)
-        #points[:,2:]=0
-        #speed[:,1:]=1
-    #except ValueError:
-    #    print('Please specify a correct source path, or create a dataset')
-    rows=points.shape[0]
+    """
+    Expected files in PATH:
+      - sampled_points.npy      (N, 2*dim)
+      - speed_mean.npy          (N, 2)
+      - speed_var.npy           (N, 2)
+      - normal_mean.npy         (N, 2*dim)
+      - normal_var.npy          (N, 2*dim)
+    """
 
+    # Load data
+    points = np.load(f'{PATH}/sampled_points.npy')
+    speed_mean = np.load(f'{PATH}/speed_mean.npy')
+    speed_var  = np.load(f'{PATH}/speed_var.npy')
+    normal_mean = np.load(f'{PATH}/normal_mean.npy')
+    normal_var  = np.load(f'{PATH}/normal_var.npy')
 
-    print(points.shape,speed.shape)
-    #print(np.shape(grid))
-    #print(XP.shape,YP.shape)
-    database = _numpy2dataset(points,speed,normal)
-    #database = _numpy2dataset(XP,YP)
-    return database
+    # Basic sanity checks (very helpful during transition)
+    assert points.shape[0] == speed_mean.shape[0] == speed_var.shape[0], \
+        "Mismatch in number of samples (points / speed)"
+    assert normal_mean.shape == normal_var.shape, \
+        "Normal mean/var shape mismatch"
 
+    print("Loaded dataset:")
+    print(" points       :", points.shape)
+    print(" speed_mean  :", speed_mean.shape)
+    print(" speed_var   :", speed_var.shape)
+    print(" normal_mean :", normal_mean.shape)
+    print(" normal_var  :", normal_var.shape)
 
+    # Convert to torch tensors
+    points = Tensor(points)
+    speed_mean = Tensor(speed_mean)
+    speed_var  = Tensor(speed_var)
+    normal_mean = Tensor(normal_mean)
+    normal_var  = Tensor(normal_var)
 
+    # Concatenate into one tensor
+    data = torch.cat(
+        (points,
+         speed_mean,
+         speed_var,
+         normal_mean,
+         normal_var),
+        dim=1
+    )
 
-
+    return _numpy2dataset(data)
